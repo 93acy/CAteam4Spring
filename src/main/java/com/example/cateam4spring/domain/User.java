333b39cc8cde5ced3e9aec05f55775a8a14c8766
chonky <acy_071093@hotmail.com>
2021-06-24 16:56:31 +0800

    User Class

--- src/main/java/com/example/cateam4spring/domain/User.java
+++ src/main/java/com/example/cateam4spring/domain/User.java
@@ -1,14 +1,41 @@
 package com.example.cateam4spring.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-public class User {
+@Data
+@NoArgsConstructor
+@Entity
+public class User{
     @Id
-    int ID;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long ID;
+    private String userName;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private RoleType role;
+    public User(String userName, String password, RoleType role
+    ){
 
-    String password;
-    String firstName;
-    String lastName;
+        this.userName= userName;
+        this.password= password;
+        this.role=role;
+    }
+
+
+    public User(String userName, String password){
+        super();
+        this.userName= userName;
+        this.password= password;
+    }
+
+
+
 }
