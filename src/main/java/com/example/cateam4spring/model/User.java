25926a8429bc586e88c02657bc19fc5d05cbde3d
chonky <acy_071093@hotmail.com>
2021-06-25 14:05:19 +0800

    Amended User Class w/ only user, password, role

--- src/main/java/com/example/cateam4spring/model/User.java
+++ src/main/java/com/example/cateam4spring/model/User.java
@@ -4,38 +4,24 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
 @Entity
+@MappedSuperclass
 public class User{
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long ID;
+
     private String userName;
     private String password;
-    private String firstName;
-    private String lastName;
     private RoleType role;
+
     public User(String userName, String password, RoleType role
     ){
-
+        super();
         this.userName= userName;
         this.password= password;
         this.role=role;
     }
-
-
-    public User(String userName, String password){
-        super();
-        this.userName= userName;
-        this.password= password;
-    }
-
-
 
 }
