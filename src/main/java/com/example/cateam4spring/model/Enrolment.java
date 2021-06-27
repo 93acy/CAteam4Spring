76c892505c2c6e7b118c1e042bfc8ab4e8ebca51
chonky <acy_071093@hotmail.com>
2021-06-27 14:29:52 +0800

    Enrolment

--- src/main/java/com/example/cateam4spring/model/Enrolment.java
+++ src/main/java/com/example/cateam4spring/model/Enrolment.java
@@ -1,6 +1,39 @@
 package com.example.cateam4spring.model;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class Enrolment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer Id;
+
+    @ManyToOne
+    private Course course;
+
+    @ManyToOne
+    private Student student;
+
+    private String courseStatus;
+    private Date startDate;
+
+    private String grade;
+
+    public Enrolment(Integer enrolId, Course courseId, Student studentId, String courseStatus, Date startDate, String grade) {
+        this.Id = enrolId;
+        this.course = courseId;
+        this.student = studentId;
+        this.courseStatus = courseStatus;
+        this.startDate = startDate;
+        this.grade = grade;
+    }
+
+    public Enrolment() {
+    }
+
     //Student-course
+
+
 }
