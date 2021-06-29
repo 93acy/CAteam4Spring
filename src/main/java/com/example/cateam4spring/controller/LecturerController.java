349bec1a00c9cdadcaaefdbb1fb2fbec660d162d
FengWan <e0696870@u.nus.edu>
2021-06-29 21:57:34 +0800

    lecturer basic function based on websecurity

--- src/main/java/com/example/cateam4spring/controller/LecturerController.java
+++ src/main/java/com/example/cateam4spring/controller/LecturerController.java
@@ -1,5 +1,64 @@
 package com.example.cateam4spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.cateam4spring.repo.CourseRepository;
+import com.example.cateam4spring.repo.EnrolmentRepository;
+
+
+
+@Controller
+@RequestMapping("/lecturer")
 public class LecturerController {
+	
+	@Autowired
+	CourseRepository crepo;
+	
+	@Autowired
+	EnrolmentRepository erepo;
+	
+
+	
+	@GetMapping("/home")
+	public String returnHome() {					
+		
+		return "lecturer_home";	
+	}
+	
+	@GetMapping("/course")
+	public String listCourse(Model model) {				
+		model.addAttribute("courses", crepo.findAll());		
+		return "course";	
+	}
+	
+	
+	@GetMapping("/enrolment")
+	public String listEnrolment(Model model) {
+		model.addAttribute("enrolments", erepo.findAll());
+		return "enrolment";
+	}
+	
+	@GetMapping("/performance")
+	public String listPerformance(Model model) {
+		model.addAttribute("performances", erepo.findAll());
+		return "student_performance";
+	}
+	
+	@RequestMapping("/grade")
+	public String editGrades(Model model) {
+		model.addAttribute("glist", erepo.findAll());
+		return "grade";
+	}
+	
+	@RequestMapping("/logout")
+	public String logout() {
+
+		return "index";
+	}
+
 
 }
