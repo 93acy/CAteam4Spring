f0280533079d1ee29e80da13aab82f51b15caea9
Joel Yeo <joelyeo@outlook.com>
2021-07-01 11:34:35 +0800

    adminlecturer

--- src/main/java/com/example/cateam4spring/controller/AdminLecturerController.java
+++ src/main/java/com/example/cateam4spring/controller/AdminLecturerController.java
@@ -1,16 +1,22 @@
 package com.example.cateam4spring.controller;
 
 import com.example.cateam4spring.model.Lecturer;
+import com.example.cateam4spring.model.Role;
+import com.example.cateam4spring.repo.LecturerRepository;
+import com.example.cateam4spring.repo.RoleRepository;
 import com.example.cateam4spring.service.AdminLecturerService;
 import com.example.cateam4spring.service.EnrolmentService;
 import com.example.cateam4spring.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value="/admin_lecturer")
@@ -18,6 +24,13 @@ public class AdminLecturerController {
 	
 	@Autowired
 	private AdminLecturerService alservice;
+	
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired RoleRepository rrepo;
+	
+	@Autowired LecturerRepository lrepo;
 	
 	@RequestMapping("/new")
 	public String showNewLecturerPage(Model model) {
@@ -28,11 +41,23 @@ public class AdminLecturerController {
 	
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String saveLecturer(@ModelAttribute("lecturer") Lecturer lecturer) {
-	    alservice.save(lecturer);
-	     
+		String hashedPassword = passwordEncoder.encode(lecturer.getPassword());
+		lecturer.setPassword(hashedPassword);
+		alservice.save(lecturer);
+	    Role lecturerRole = rrepo.findRoleByName("LECTURER");
+	    lecturer.getRoles().add(lecturerRole);
+	    lrepo.save(lecturer);
 	    return "redirect:/admin_home/lecturer";
 	}
-
+	
+	@RequestMapping("/edit/{id}")
+	public ModelAndView showEditLecturerPage(@PathVariable(name = "id") int id) {
+	    ModelAndView mav = new ModelAndView("edit_lecturer");
+	    Lecturer lecturer = alservice.get(id);
+	    mav.addObject("lecturer", lecturer);
+	     
+	    return mav;
+	}
 }
 
 
