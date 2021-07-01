394e2db0aa8551fd82029d657acc88227ecc1b58
Joel Yeo <joelyeo@outlook.com>
2021-07-01 11:05:55 +0800

    adminlecturer

--- src/main/java/com/example/cateam4spring/controller/AdminLecturerController.java
+++ src/main/java/com/example/cateam4spring/controller/AdminLecturerController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value="/admin_lecturer")
@@ -18,11 +20,17 @@ public class AdminLecturerController {
 	private AdminLecturerService alservice;
 	
 	@RequestMapping("/new")
-	public String showNewProductPage(Model model) {
+	public String showNewLecturerPage(Model model) {
 	    Lecturer lecturer = new Lecturer();
 	    model.addAttribute("lecturer", lecturer);
+	    return "Admin/new_lecturer";
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveLecturer(@ModelAttribute("lecturer") Lecturer lecturer) {
+	    alservice.save(lecturer);
 	     
-	    return "new_product";
+	    return "redirect:/admin_home/lecturer";
 	}
 
 }
